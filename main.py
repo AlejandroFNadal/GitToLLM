c3a332dc2eb59861143a8ebf4f4798e38ac32e86
Alejandro Nadal <alejandro.nadal@synics.ch>
2025-05-10 15:29:09 +0200

    initial

---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+from pathlib import Path
+from typing import List, Tuple
+from sentence_transformers import SentenceTransformer
+from chromadb import PersistentClient 
+from chromadb.config import Settings
+from tree_sitter_language_pack import SupportedLanguage, get_language, get_parser
+from tree_sitter import Node
+
+LANGUAGES = {
+  '.py': 'python',
+  '.js': 'javascript',
+  '.ts': 'typescript',
+  '.html': 'html',
+  '.json': 'json',
+  '.md': 'markdown',
+  '.rs': 'rust'
+}
+
+def get_function_name(node: Node) -> str:
+    for child in node.children:
+        if child.type == 'identifier':
+            if child.text:
+                return child.text.decode('utf-8')
+    return '<anonymous>'
+
+def extract_functions(source_code: str, lang: SupportedLanguage) -> List[Tuple[str, str]]:
+    parser = get_parser(lang)
+    tree = parser.parse(bytes(source_code, 'utf8'))
+    root = tree.root_node
+    functions = []
+    node_types = set()
+
+    def walk(node: Node, context: str| None = None):
+        node_types.add(node.type)
+        if node.type in ("impl_item", "trait_item"):
+            type_name = None
+            for child in node.children:
+                if child.type == 'type_identifier':
+                    if child.text:
+                        type_name = child.text.decode('utf-8')
+                        break
+            context = type_name
+        if node.type in ("function_item"):
+            # Extract function name
+            function_name = get_function_name(node)
+            qualified_name = f"{context}::{function_name}" if context else function_name
+            code = source_code[node.start_byte:node.end_byte]
+            # Add qualified name to the function
+            code = qualified_name + "\n" + code
+            functions.append((code, qualified_name))
+        for child in node.children:
+            walk(child, context=context)
+    walk(root)
+    return functions
+
+def load_repo_text(repo_path):
+    #supported_extensions = ['.py', '.md', '.txt', '.rs', '.js', '.ts', '.html', '.json']
+    supported_extensions = ['.rs']
+    folders_to_ignore = ['.git', 'node_modules', 'target', '__pycache__', 'dist', 'build', 'venv', 'env', 'out', 'lib', 'libs', 'app', '.next']
+    chunks = []
+    for file in Path(repo_path).rglob('*'):
+        use_file = True
+        if file.is_file() == False:
+            use_file = False
+        if file.name.startswith('.'):
+            use_file = False
+        for folder in folders_to_ignore:
+            if folder in file.parts:
+                use_file = False
+                break
+        if use_file == False:
+            continue
+        if file.suffix.lower() in supported_extensions:
+            print(f"Loading {file}")
+            text = file.read_text(encoding='utf-8', errors='ignore')
+            functions = extract_functions(text, LANGUAGES[file.suffix])
+            for function in functions:
+                chunks.append({
+                    "content": function[0],
+                    "metadata": {"file": str(file), "function": function[1] }
+                })
+    print(f"Loaded {len(chunks)} chunks from {repo_path}")
+    return chunks
+
+def load_repo_text_dumb(repo_path):
+    supported_extensions = ['.py', '.md', '.txt', '.rs', '.js', '.ts',]
+    folders_to_ignore = ['.git', 'node_modules', 'target', '__pycache__', 'dist', 'build', 'venv', 'env', 'out', 'lib', 'libs', 'app', '.next']
+    chunks = []
+    for file in Path(repo_path).rglob('*'):
+        use_file = True
+        if file.is_file() == False:
+            use_file = False
+        if file.name.startswith('.'):
+            use_file = False
+        for folder in folders_to_ignore:
+            if folder in file.parts:
+                use_file = False
+                break
+        if use_file == False:
+            continue
+        if file.suffix.lower() in supported_extensions:
+            print(f"Loading {file}")
+            text = file.read_text(encoding='utf-8', errors='ignore')
+            # Split by sections/lines or tokens (can be smarter)
+            parts = [text[i:i+1000] for i in range(0, len(text), 1000)]
+            for part in parts:
+                chunks.append({
+                    "content": part,
+                    "metadata": {"file": str(file)}
+                })
+    print(f"Loaded {len(chunks)} chunks from {repo_path}")
+    return chunks
+
+
+if __name__ == "__main__":
+    chunks = load_repo_text("/home/ale/Documents/asisCHARTSx")
+    print(chunks)
+    """
+    texts = [chunk["content"] for chunk in chunks]
+    metadatas = [chunk["metadata"] for chunk in chunks]
+    ids = [f"chunk_{i}" for i in range(len(chunks))]
+    # Initialize the ChromaDB Client
+    client = PersistentClient(path="./db")
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    # Create a collection
+    collection = client.create_collection(name="repo")
+    embeddings = model.encode(texts, show_progress_bar=True, batch_size=32).tolist()
+    collection.add(
+        documents=texts,
+        metadatas=metadatas,
+        ids=ids,
+        embeddings=embeddings
+    )
+    """
---
+++ question.py
@@ -0,0 +1,51 @@
+import requests
+import sys
+
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+from chromadb import PersistentClient
+
+chroma = PersistentClient(path="./db")
+collection = chroma.get_collection(name="repo")
+
+def run_question(question: str):
+    query_embedding = model.encode(question).tolist()
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=5
+    )
+
+    if results['documents'] == []:
+        print("No results found.")
+        return
+    if results['documents']:
+        docs = results['documents'][0]
+        context = "\n---\n".join(docs)
+
+        prompt = f"""
+        You are an AI assistant that answers questions about the codebase 
+
+        ### Context:
+        {context}
+
+        ### Question:
+        {question}
+
+        ### Answer:
+
+        """
+        response = requests.post(
+            "http://localhost:11434/api/generate",
+            json={
+                "model": "codellama",
+                "prompt": prompt,
+                "stream": False
+            }
+        )
+        answer = response.json()
+        print(f"Answer: {answer['response']}")
+
+if __name__ == "__main__":
+    run_question(sys.argv[1])
+
